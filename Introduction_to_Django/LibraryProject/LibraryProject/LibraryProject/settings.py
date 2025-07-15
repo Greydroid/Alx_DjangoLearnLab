f23ae7f7f3013d0e82b5d37fceb58ab550d4dc54
Greydroid <heleneelegalam@gmail.com>
2025-07-15 14:53:29 +0100

    added libraryproject again

--- Introduction_to_Django/LibraryProject/LibraryProject/LibraryProject/settings.py
+++ Introduction_to_Django/LibraryProject/LibraryProject/LibraryProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s6%ptbp+&6zjbb-$#xip66+6kqbq$_1u*xc@tx_(d5rldbn^ye'
+SECRET_KEY = 'django-insecure-(e-zm)_phtco(o2w+)lp0t(8yva1mmz6knzi1zhbaro5!&kuxj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
