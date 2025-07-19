97530f76fc20088cc5a394e7fe59410f9eb576b0
Greydroid <heleneelegalam@gmail.com>
2025-07-19 09:08:34 +0100

    added library name to variable

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -19,7 +19,9 @@ for book in books_by_author:
     print("-", book.title)
 
 # List all books in a library
-library = Library.objects.get(name="Central Library")
+library_name = "Central Library"
+library = Library.objects.get(name=library_name)
+
 books_in_library = library.books.all()
 print(f"\n Books in {library.name}:")
 for book in books_in_library:
