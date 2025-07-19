ba24f22aa22d0207fa205f1b98237410634eb065
Greydroid <heleneelegalam@gmail.com>
2025-07-19 18:00:11 +0100

    Implement custom permissions

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -21,12 +21,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('relationship_app.urls')),
 ]
-
-from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('relationship_app.urls')),  # include app URLs
+    path('', include('relationship_app.urls')),
 ]
 
