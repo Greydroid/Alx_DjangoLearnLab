04259e0b1b266fc37c0a1ebfb5ae07e0fbd65ab1
Greydroid <heleneelegalam@gmail.com>
2025-07-19 11:02:39 +0100

    added urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -12,7 +12,7 @@ from .views import register_view
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('register/', register_view, name='register'),
+    path('register/', views.register_view, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
