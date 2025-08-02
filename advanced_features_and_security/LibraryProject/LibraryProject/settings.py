c3ca7f648152e64a263dff401b704228b8d92f90
Greydroid <heleneelegalam@gmail.com>
2025-08-02 01:05:39 +0100

    implementing django security best practices

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -174,6 +174,10 @@ SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
+# SECURITY: Trust X-Forwarded-Proto header set by proxy for secure requests
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+
 # HSTS (Strict Transport Security)
 SECURE_HSTS_SECONDS = 31536000
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
